91682ac11fe03a7cc287a0367a0df80136ff9e1e
Simon Kraft <s49skraf@uni-bonn.de>
2026-03-12 16:41:23 -0700

    implement source file for tarjan algo

---
+++ include/scc.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "sparse.hpp"
+#include <vector>
+#include <functional>
+
+namespace Sparselib {
+
+namespace SCC {
+
+    // Returns a list of strongly connected components; each component is a vector of vertex indices
+    // CSC matrix is interpreted as outgoing edges of vertex u are in column u
+    std::vector<std::vector<int>> tarjanSCC(const Sparse::CSCMatrix& matrix);
+
+
+}
+}
